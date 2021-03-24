f6e73e0f2d61f98322ce06d30384d46c35c7f7f6
Sana <sanaahmad393@gmail.com>
2021-03-24 16:20:22 +0000

    Updated seed_database.py

--- seed_database.py
+++ seed_database.py
@@ -10,3 +10,32 @@ import model
 import server
 
 os.system('dropdb ratings')
+os.system('createdb ratings')
+
+model.connect_to_db(server.app)
+model.db.create_all()
+
+#Load movie data from JSON file
+with open('data/movies.json') as f:
+    movie_data = json.loads(f.read())
+
+# Create fake movies, store them in a list so we can use them 
+# to later create fake ratings
+movies_in_db = []
+for movie in movie_data:
+
+    title, overview, poster_path = (movie['title'],
+                                   movie['overview'],
+                                   movie['poster_path'])
+    
+    release_date = datetime.strptime(movie['release_date'], '%Y-%m-%d')
+
+    db_movie = crud.create_movie(title, 
+                                overview, 
+                                release_date, 
+                                poster_path)
+
+    movies_in_db.append(db_movie)
+
+
+
